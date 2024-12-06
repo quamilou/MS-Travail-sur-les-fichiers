be1e9adf748726c449a8ffb5ef5c64d5bb2675ed
quamilou <quamilougame@gmail.com>
2024-12-06 16:19:35 +0100

    Update MS-Travail-sur-les-fichiers.AssemblyInfo.cs

--- obj/Debug/net8.0/MS-Travail-sur-les-fichiers.AssemblyInfo.cs
+++ obj/Debug/net8.0/MS-Travail-sur-les-fichiers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MS-Travail-sur-les-fichiers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8c6444f890f41d74acfc187812e472f8caf1f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa70055f0efb459e37ac5dedd1e32e9402fbfe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MS-Travail-sur-les-fichiers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MS-Travail-sur-les-fichiers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
